b4b44673c3bd0e1c86f9333b30fce569d67eba34
Brent Vollebregt <volleys2468@gmail.com>
2018-03-20 10:35:32 +1300

    Images now display and are added when selected

--- static/script.js
+++ static/script.js
@@ -1,49 +1,51 @@
 let data;
 let current = null;
 
-// async function setImages() {
-//     // TODO Loading bar
-//     data = await eel.getImageData()();
-//     let parent = document.getElementById('scroll_bar');
-//     parent.style.width = window.innerWidth-40 + 'px';
-//     for (const key of Object.keys(data)) {
-//         let div = document.createElement('div');
-//         let img = document.createElement('img');
-//         div.style.display = 'inline-block';
-//         img.style.height = document.getElementById('scroll_bar').clientHeight + 'px';
-//         img.style.border = '2px solid #458BC6';
-//         img.style.margin = '0 2px';
-//         img.src = 'data:image/jpg;base64,' + data[key]['base64_thum'];
-//         img.onclick = function () { setAsMain(key)};
-//         div.appendChild(img);
-//         parent.appendChild(div);
-//     }
-//     setAsMain('1');
-// }
-//
-// function setAsMain(id) {
-//     document.getElementById('main_img').src = 'data:image/jpg;base64,' + data[id]['base64'];
-//     document.getElementById('info_date').innerHTML = data[id]['date'];
-//     document.getElementById('info_size').innerHTML = data[id]['size'] + "Mb";
-//     document.getElementById('info_dimensions').innerHTML = 'N/A';
-//     document.getElementById('info_model').innerHTML = data[id]['model'];
-//     document.getElementById('info_location').innerHTML = 'N/A';
-//     current = id;
-// }
-//
-// // setImages();
-// function call(event) {
-//     document.getElementById('main_img').src = URL.createObjectURL(event.target.files[0]);
-//     console.log(event.target.files[0].name);
-// }
-
 // Setup Images
 function setupImages(images_data) {
-    console.log(images_data);
-    if (current === null) {
-        // TODO Set the current image
-    }
+    if (images_data === null) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('get', '/getImages/', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.responseType = 'json';
+        xhr.onload = function() {
+            setupImages(this.response);
+        };
+        xhr.send();
+    } else {
+        data = images_data;
+        let parent = document.getElementById('scroll_bar');
+        parent.style.width = window.innerWidth-40 + 'px';
+        for (const key of Object.keys(data)) {
+            // TODO Make sure images aren't already there
+            let div = document.createElement('div');
+            let img = document.createElement('img');
+            div.style.display = 'inline-block';
+            div.style.cursor = 'pointer';
+            img.style.height = document.getElementById('scroll_bar').clientHeight + 'px';
+            img.style.border = '2px solid #458BC6';
+            img.style.margin = '0 2px';
+            img.src = '/image/' + key;
+            img.onclick = function () { setMain(key) };
+            div.appendChild(img);
+            parent.appendChild(div);
+        }
 
+        // TODO Setup images
+        if (current === null) {
+            setMain('1');
+        }
+    }
+}
+
+function setMain(id) {
+    document.getElementById('main_img').src = '/image/' + id;
+    document.getElementById('info_date').innerHTML = 'N/A';
+    document.getElementById('info_size').innerHTML = 'N/A';
+    document.getElementById('info_dimensions').innerHTML = 'N/A';
+    document.getElementById('info_model').innerHTML = 'N/A';
+    document.getElementById('info_location').innerHTML = 'N/A';
+    current = id;
 }
 
 // Dialogs
@@ -82,3 +84,7 @@ function getImagesWithFileSelect() {
     };
     xhr.send();
 }
+
+window.addEventListener('load', function () {
+    setupImages(null);
+});
