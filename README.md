428eef82f7eb349734a1bb09850e7dc072c11ba5
Brent Vollebregt <volleys2468@gmail.com>
2018-09-05 15:45:18 +1200

    Merged run.py and utils.py and edited README

--- README.md
+++ README.md
@@ -7,6 +7,20 @@ I had been requested to help someone with filtering a lot of images taken with a
 
 This project is a Flask server hosted by Python that is designed to be opened in a Chrome window. Other browsers will work but due to layouts, images of different sizes will mess with the display.
 
+## Getting Started
+
+### Prerequisites
+ - Python : Python 2.7, 3.4 - 3.6
+ - Chrome : to run the user interface
+
+### Usage
+1. Clone/download this repo
+2. Open cmd/terminal and cd to the project
+3. Execute pip install -r requirements.txt
+4. Execute `run.py` using `python run.py` or a similar method
+
+Click the blue "Add Files" button and add your desired files. Use the red/green button to select what images to keep and then click export when you have finished your selection (you do not need to select all the images to do this, blue will be ignored like red).
+
 ## Features
  - Add files by file, files or folder
  - Simple "Copy" or "Don't Copy" buttons to determine what to keep
@@ -17,19 +31,3 @@ This project is a Flask server hosted by Python that is designed to be opened in
  - Clicking to the right/left of the image will go to the next corresponding image
  - View all images in a scrollable bar at the bottom
  - Server will close itself when it detects that there is no more activity
-
-## Getting Started
-
-### Prerequisites
- - Python : Python 2.7, 3.4 - 3.6
- - Chrome : to run the user interface
-
-### Instillation
-1. Clone/download this repo
-2. Open cmd/terminal and cd to the project
-3. Execute pip install -r requirements.txt
-
-### Usage
-Execute ```run.py``` by either calling it as an argument with Python, in IDLE or double clicking on it. Chrome should open and display the interface.
-
-Click the blue "Add Files" button and add your desired files. Use the red/green button to select what images to keep and then click export when you have finished your selection (you do not need to select all the images to do this, blue will be ignored like red).
--- run.py
+++ run.py
@@ -4,20 +4,112 @@ import os
 import sys
 import time
 import threading
-import utils
 import webbrowser
+from shutil import copyfile
+from PIL import Image
+if sys.version_info.major >= 3:
+    from tkinter import Tk
+    from tkinter.filedialog import askdirectory, askopenfilenames
+else:
+    from Tkinter import Tk
+    try:
+        from Tkinter.filedialog import askdirectory, askopenfilenames
+    except:
+        from tkFileDialog import askdirectory as askdirectory
+        from tkFileDialog import askopenfilenames as askopenfilenames
+
+
+supported_extensions = ['.jpg', '.jpeg', '.png', '.svg', '.bmp', '.ico', '.gif']
+
+
+def select_directory():
+    """ Opens a select directory dialog and returns the path selected """
+    root = Tk()
+    root.withdraw()
+    root.wm_attributes('-topmost', 1)
+    directory = askdirectory(parent=root)
+    return directory
+
+
+def select_files():
+    """ Opens a select files dialog and returns the files selected """
+    root = Tk()
+    root.withdraw()
+    root.wm_attributes('-topmost', 1)
+    files = askopenfilenames(parent=root,
+                             title="Select file",
+                             filetypes=(("Image files", '*' + ';*'.join(supported_extensions)), ("all files", "*.*"))
+                             )
+    return root.tk.splitlist(files)
+
+
+def get_files_from_directory(directory):
+    """ Gets files with supported file extensions within a directory """
+    file_list = []
+    for root, dirs, files in os.walk(directory.replace('/', '\\'), topdown=False):
+        for name in files:
+            if os.path.splitext(name)[1] in supported_extensions:
+                file_list.append(os.path.join(root, name))
+    return file_list
+
+
+def add_images_to_dict(image_dict, image_list):
+    """ Adds a new image to a dictionary object. image_dict[next_index] = {'location' : location...}"""
+    if len(image_dict) > 0:
+        next_value = max([int(i) for i in image_dict]) + 1
+    else:
+        next_value = 1
+    files = [image_dict[i]['location'] for i in image_dict]
+    for image in image_list:
+        if image not in files:
+            image_dict[str(next_value)] = {
+                'location' : image,
+                'size' : round(os.path.getsize(image) / 1000000, 2),
+                'date' : get_exif(image, 'date'),
+                'model' : get_exif(image, 'model')
+            }
+            next_value += 1
+
+
+def get_exif(file, data):
+    try: # Also handles if exif is not None
+        exif = Image.open(file)._getexif()
+        if data == 'date' and 36867 in exif:
+            return exif[36867]
+        if data == 'model' and 272 in exif:
+            return exif[272]
+    except:
+        pass
+    return 'N/A'
+
+
+def copy_media(items, dest):
+    """ Copies files from their location to a new destination """
+    for file in items:
+        filename = os.path.basename(file)
+        copyfile(file, dest + '\\' + filename)
+
+
+def move_media(items, dest):
+    """ Moves files from their location to a new destination """
+    for file in items:
+        filename = os.path.basename(file)
+        os.rename(file, dest + '\\' + filename)
+
 
 images = {}
 
 # Get Args
 input_folder = ''
 if len(sys.argv) > 1:
-    image_files = utils.getFilesFromDirectory(sys.argv[1])
-    utils.addImagesToDict(images, image_files)
+    image_files = get_files_from_directory(sys.argv[1])
+    add_images_to_dict(images, image_files)
+
 
 class PingThread(threading.Thread):
     timeout = 0
     EXTEND_TIME = 60
+
     def __init__(self):
         super(PingThread, self).__init__()
         self.refresh()
@@ -49,15 +141,15 @@ def getImageRoute(id):
 
 @app.route('/selectDirectory')
 def selectDirectoryRoute():
-    directory = utils.selectDirectory()
-    files = utils.getFilesFromDirectory(directory)
-    utils.addImagesToDict(images, files)
+    directory = select_directory()
+    files = get_files_from_directory(directory)
+    add_images_to_dict(images, files)
     return jsonify(images)
 
 @app.route('/selectFiles')
 def selectFilesRoute():
-    files = utils.selectFiles()
-    utils.addImagesToDict(images, files)
+    files = select_files()
+    add_images_to_dict(images, files)
     return jsonify(images)
 
 @app.route('/clearImages')
@@ -69,9 +161,9 @@ def clearImagesRoute():
 def exportCopyRoute():
     items = request.json
     try:
-        output_dir = utils.selectDirectory()
+        output_dir = select_directory()
         file_locations = [images[i]['location'] for i in items]
-        utils.copyMedia(file_locations, output_dir)
+        copy_media(file_locations, output_dir)
     except Exception as e:
         return jsonify({'success': False, 'message' : str(e)})
     return jsonify({'success': True})
@@ -80,9 +172,9 @@ def exportCopyRoute():
 def exportMoveRoute():
     items = request.json
     try:
-        output_dir = utils.selectDirectory()
+        output_dir = select_directory()
         file_locations = [images[i]['location'] for i in items]
-        utils.moveMedia(file_locations, output_dir)
+        move_media(file_locations, output_dir)
     except Exception as e:
         return jsonify({'success': False, 'message': str(e)})
     return jsonify({'success': True})
